d93646a156928d1cba776eec33aa56f0d047961c
mebylund <35643665+mebylund@users.noreply.github.com>
2018-08-30 19:58:44 -0700

    upload game

--- Frogger-Game/js/app.js
+++ Frogger-Game/js/app.js
@@ -6,7 +6,7 @@ class Enemy {
         this.x=x;
         this.y=y;
         this.speed=speed;
-         // The image/sprite for our enemies, this uses
+        // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
     }
@@ -19,12 +19,12 @@ class Enemy {
             player.resetPosition();
             player.points = player.points - 1;
             player.life = player.life - 1;
+            player.gameOver();
         }
+        
     };
 
 }
-
-
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -61,21 +61,30 @@ class Player{
         this.life = 3;
         this.lifeImage = 'images/Heart.png';
     }
-    update() {
+    update() { //updates player possitoin and prevents moving off screen
+        if (this.x > 400){
+            this.x = 400;
+        }
+        if (this.x < 0){
+            this.x = 0;
+        }
+        if (this.y > 400){
+            this.y = 400;
+        }
         if (this.y <= 0){
             this.points += 3;
             player.resetPosition();
         }
     }
-    drawPointsImage(){
-        ctx.drawImage(Resources.get(this.pointsImage), 0, 50, 50, 50);
+    drawPointsImage(){ 
+        ctx.drawImage(Resources.get(this.pointsImage), 0, 50, 50, 50);// (x,y,hight,width)
         ctx.fillText(this.points, 65, 75);
     }
-    drawLifeImage(){
-        ctx.drawImage(Resources.get(this.lifeImage), 415, 50, 50, 50);
+    drawLifeImage(){ 
+        ctx.drawImage(Resources.get(this.lifeImage), 415, 50, 50, 50);//(x,y,hight,width)
         ctx.fillText(this.life, 470, 75);
     }
-    render(){
+    render(){ //puts player on the screen along with score and life
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
         ctx.fillStyle = 'white';
         ctx.font = "16px Arial";
@@ -100,19 +109,28 @@ class Player{
         this.x=200;
         this.y=400;
     }
+    restartGame(){
+        this.points = 0;
+        this.life = 3;
+    }
+    gameOver(){
+        if (this.life === 0){
+            alert ("Game over! You got " + this.points + " point(s)!");
+            this.restartGame();
+        }
+    }
 }
-var player = new Player(200,400,50);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
 var allEnemies = [];
 let enemyY = [40, 130, 220];
 enemyY.forEach(function(Y) {
     var enemy = new Enemy(-100, Y, Math.random() * 500 + 80);
     allEnemies.push(enemy);
 });
+var player = new Player(200,400,50);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -123,7 +141,6 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
